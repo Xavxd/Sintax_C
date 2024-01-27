614925f7e29eea058c38811fb2c1a978176d11a2
Xavpunkxd <jon20xavi@gmail.com>
2024-01-27 12:43:49 -0600

    reconect

---
+++ CPP/sintaxis.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+
+/* el (std) se puede omitir en el codigo usando este comando pero es mejor usarlo 
+directamente en el codigo debido a que a veces usarlo de esta manera genera errores y puede considerarse una mala practica 
+using namespace std;*/
+
+int main() {
+    //aqui para mostrar en pantalla se usa "cout"
+    std :: cout<<"Hello world"<< std:: endl;
+
+    //tipos de variables:
+    int i;
+    /*en este lenguaje si se puede usar la variable string pero hay que aclarar que se va hacer uso de ello con "std"*/
+    std :: string s;
+    return 0;
+}
